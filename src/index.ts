141026e604afac76d4694597c1b4852ad0c2e671
kushagra1201 <imt_2019053@iiitm.ac.in>
2024-04-17 01:55:09 +0530

    added path related functions for git clones

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import express from "express";
 import cors from "cors";
-import { nanoid } from "nanoid";
 import simpleGit from "simple-git";
+import { generate } from "./utils";
+import { getAllFiles } from "./file";
+import path from "path";
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 app.post("/deploy", async (req, res) => {
   const repoUrl = req.body.repoUrl;
-  const id = nanoid();
-  await simpleGit().clone(repoUrl, `output/${id}`);
+  const id = generate();
+  await simpleGit().clone(repoUrl, path.join(__dirname, `output/${id}`));
+
+  const files = getAllFiles(path.join(__dirname, `output/${id}`));
+  console.log(files);
 
   res.json({
     id: id,
