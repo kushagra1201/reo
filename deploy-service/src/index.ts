99f38ae2f79fe2c0520371de8a1227b08539d8fe
kushagra1201 <kushagra.nageshwar2001@gmail.com>
2024-04-23 02:43:37 +0530

    restructuring project

---
+++ deploy-service/src/index.ts
@@ -0,0 +1,22 @@
+import { commandOptions, createClient } from "redis";
+import { downloadS3Folder } from "./aws";
+import { buildProject } from "./utils";
+const subscriber = createClient();
+
+subscriber.connect();
+
+async function main() {
+  while (1) {
+    const response = await subscriber.brPop(
+      commandOptions({ isolated: true }),
+      "build-queue",
+      0
+    );
+    // @ts-ignore
+    const id = response.element;
+    await downloadS3Folder(`output/${id}`);
+    await buildProject(id);
+  }
+}
+
+main();
